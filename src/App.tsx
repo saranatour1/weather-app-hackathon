8e3b40ff3ef637a630d23449549ca6ab1b1eb9e9
saranatour1 <saranatourcontact@gmail.com>
2025-09-10 18:10:03 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import '@fontsource-variable/dm-sans';
+function App() {
+
+  return (
+    <p className='text-preset-1'>The quick brown fox jumps over the lazy dog.</p>
+  )
+}
+
+export default App
