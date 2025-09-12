c89ce5645e593af6c67dd8e49a3e48e504f28669
saranatour1 <saranatourcontact@gmail.com>
2025-09-12 17:22:07 +0200

    feat: add Retry icon and update CSS variables for weather backgrounds

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,3 @@
-import { Logo, Search, Settings, Forbidden, CheckMark, Loading, ArrowDown } from "./Icons";
+import { Logo, Search, Settings, Forbidden, CheckMark, Loading, ArrowDown, Retry } from "./Icons";
 
-export { Logo, Search, Settings, Forbidden, CheckMark, Loading, ArrowDown }
+export { Logo, Search, Settings, Forbidden, CheckMark, Loading, ArrowDown, Retry }
